71441e574edee5351344148c5c82b9775f2a5f47
curlygecko <dogu2620@gmail.com>
2018-08-17 01:02:16 +0300

    update

--- app/src/main/java/com/example/dogu2/knowledgequiz/MainActivity.java
+++ app/src/main/java/com/example/dogu2/knowledgequiz/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.dogu2.knowledgequiz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
@@ -12,21 +13,21 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends Activity {
 
-    TextView sorulanSoru, yanlisCevap, dogruCevap;
+    TextView sorulanSoru, dogruCevap, hakk;
     RadioGroup soruGrubu;
     RadioButton cevap1, cevap2, cevap3, cevap4;
     Random random;
     int number;
-    List<RadioButton> secenekler;
-    int secenek, skor, secenek1, secenek2, secenek3;
-    String sorulan,score;
+    ArrayList<String> secenekler;
+    public int p1, p2 , p3, p4,skor,kalanHak;
+    String sorulan,skorr, kalanHakk;
     boolean dongu=true;
 
     @Override
@@ -34,57 +35,96 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final questions quiz = new questions();
-
-        random = new Random();
-
-        number=0;
+        Intent intent = new Intent(this, MainActivity.class);
+        number=1;
+        p1=0;
+        p2=1;
+        p3=2;
+        p4=3;
         skor=0;
+        skorr="";
+        kalanHak=3;
+        kalanHakk="";
 
-        secenekler = new ArrayList<>();
+        secenekler = new ArrayList<String>();
         sorulanSoru = findViewById(R.id.soru);
-
         dogruCevap = findViewById(R.id.dogru);
-
+        hakk = findViewById(R.id.hak);
+        hakk.setText(Integer.toString(kalanHak));
         soruGrubu = findViewById(R.id.secenekler);
         cevap1 = findViewById(R.id.secenek1);
         cevap2 = findViewById(R.id.secenek2);
         cevap3 = findViewById(R.id.secenek3);
         cevap4 = findViewById(R.id.secenek4);
 
-        secenekler.add(cevap1);
-        secenekler.add(cevap2);
-        secenekler.add(cevap3);
-        secenekler.add(cevap4);
+
+        secenekler.add(quiz.getCevap(number));
+        secenekler.add(quiz.getOthers1(number));
+        secenekler.add(quiz.getOthers2(number));
+        secenekler.add(quiz.getOthers3(number));
+        Collections.shuffle(secenekler);
 
         sorulanSoru.setText(quiz.getSoru(number));
-        secenek = random.nextInt(secenekler.size());
-        secenekler.get(secenek).setText(quiz.getCevap(number));
+        cevap1.setText(secenekler.get(p1));
+        cevap2.setText(secenekler.get(p2));
+        cevap3.setText(secenekler.get(p3));
+        cevap4.setText(secenekler.get(p4));
+
 
         soruGrubu.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
                 RadioButton secilen = (RadioButton) soruGrubu.findViewById(soruGrubu.getCheckedRadioButtonId());
                 if(secilen.getText().equals(quiz.getCevap(number))) {
-                    secenekler.get(secenek).setText("");
+                    hakk.setText(kalanHakk);
+                    skor++;
+                    skorr = Integer.toString(skor);
+                    p1=0;
+                    p2=1;
+                    p3=2;
+                    p4=3;
+                    secenekler.clear();
+                    cevap1.setText("");
+                    cevap2.setText("");
+                    cevap3.setText("");
+                    cevap4.setText("");
+                    soruGrubu.clearAnimation();
                     secilen.setChecked(false);
-                    secenekler.get(secenek).clearAnimation();
                     number++;
                     sorulanSoru.setText(quiz.getSoru(number));
-                    secenek = random.nextInt(secenekler.size());
-                    secenek1 = random.nextInt(secenekler.size());
-                    secenek2 = random.nextInt(secenekler.size());
-                    secenek3 = random.nextInt(secenekler.size());
-                    secenekler.get(secenek).setText(quiz.getCevap(number));
-                    secenekler.get(secenek1).setText(quiz.getCevap(number));
-                    secenekler.get(secenek2).setText(quiz.getCevap(number));
-                    secenekler.get(secenek3).setText(quiz.getCevap(number));
-                }}
+                    secenekler.add(quiz.getCevap(number));
+                    secenekler.add(quiz.getOthers1(number));
+                    secenekler.add(quiz.getOthers2(number));
+                    secenekler.add(quiz.getOthers3(number));
+                    Collections.shuffle(secenekler);
+                    cevap1.setText(secenekler.get(p1));
+                    cevap2.setText(secenekler.get(p2));
+                    cevap3.setText(secenekler.get(p3));
+                    cevap4.setText(secenekler.get(p4));
+                    dogruCevap.setText("SKOR: "+skorr);
+                }else if(!secilen.getText().equals(quiz.getCevap(number))) {
+                    kalanHak--;
+                    kalanHakk = Integer.toString(kalanHak);
+                    hakk.setText(kalanHakk);
+                    if(kalanHak <= 0){
+                        sorulanSoru.setText("OYUN BİTTİ");
+                        cevap1.setEnabled(false);
+                        cevap2.setEnabled(false);
+                        cevap3.setEnabled(false);
+                        cevap4.setEnabled(false);
+                    }
+
+                }
+
+            }
         });
     }
-        //questions sorucevap = new questions();
-        //sorulanSoru.setText(sorucevap.getSoru(number));
-        //secenek = random.nextInt(secenekler.size());
-        //secenekler.get(secenek).setText(sorucevap.getCevap(number));
 
-
+    public void delay(){
+        try {
+            TimeUnit.MILLISECONDS.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- app/src/main/java/com/example/dogu2/knowledgequiz/questions.java
+++ app/src/main/java/com/example/dogu2/knowledgequiz/questions.java
@@ -12,46 +12,80 @@ import java.util.Set;
 
 public class questions {
 
-    ArrayList<String> sorular, cevaplar, soru1;
+    HashMap<Integer, String> sorular, cevaplar, others1, others2, others3;
     String soruu;
-    String cevap;
+    String cevap,other1,other2, other3;
 
     public questions() {
-        sorular = new ArrayList<String>();
-        sorular.add("Fatih Sultan Mehmet'in babası kimdir?");
-        sorular.add("Magna Carta sözleşmesi hangi ülke ile yapılmıştır?");
-        sorular.add("Hangisi bir doğal sayıdır?");
-        sorular.add("Hangi ülke Asya kıtasındadır ?");
-        sorular.add("Hangi hayvan memeli değildir ?");
-        sorular.add("Hangisi periyodik tabloda bulunan bir element değildir?");
-        sorular.add("Hangisi tarihteki Türk devletlerinden biri değildir?");
+        sorular = new HashMap<>();
+        sorular.put(1,"Fatih Sultan Mehmet'in babası kimdir?");
+        sorular.put(2,"Magna Carta sözleşmesi hangi ülke ile yapılmıştır?");
+        sorular.put(3,"Hangisi bir doğal sayıdır?");
+        sorular.put(4,"Hangi ülke Asya kıtasındadır ?");
+        sorular.put(5,"Hangi hayvan memeli değildir ?");
+        sorular.put(6,"Hangisi periyodik tabloda bulunan bir element değildir?");
+        sorular.put(7,"Hangisi tarihteki Türk devletlerinden biri değildir?");
 
-        cevaplar = new ArrayList<String>();
-        cevaplar.add("2.Murat");
-        cevaplar.add("İngiltere");
-        cevaplar.add("0");
-        cevaplar.add("Singapur");
-        cevaplar.add("Penguen");
-        cevaplar.add("Su");
-        cevaplar.add("Emevi Devleti");
+        cevaplar = new HashMap<>();
+        cevaplar.put(1,"2.Murat");
+        cevaplar.put(2,"İngiltere");
+        cevaplar.put(3,"0");
+        cevaplar.put(4,"Singapur");
+        cevaplar.put(5,"Penguen");
+        cevaplar.put(6,"Su");
+        cevaplar.put(7,"Emevi Devleti");
 
+        others1 = new HashMap<>();
+        others1.put(1,"8.Osman");
+        others1.put(2,"Almanya");
+        others1.put(3,"-7");
+        others1.put(4,"Tayland");
+        others1.put(5,"Ayı");
+        others1.put(6,"Karbon");
+        others1.put(7,"Moğol İmparatorluğu");
 
+        others2 = new HashMap<>();
+        others2.put(1,"Kanuni Süleyman");
+        others2.put(2,"Kanada");
+        others2.put(3,"3,14");
+        others2.put(4,"Kongo");
+        others2.put(5,"Muhabbet Kuşu");
+        others2.put(6,"Azot");
+        others2.put(7,"Roma İmparatorluğu");
+
+        others3 = new HashMap<>();
+        others3.put(1,"5.Süleyman");
+        others3.put(2,"İtalya");
+        others3.put(3,"0,1");
+        others3.put(4,"Rusya");
+        others3.put(5,"Yunus");
+        others3.put(6,"Krypton");
+        others3.put(7,"Çin İmparatorluğu");
     }
 
     public String getSoru(int sira){
-        if(sira >= sorular.size()){
-            return  "OYUN BİTTİ";
-        }else
-            soruu = sorular.get(sira);
-            return soruu;
+        soruu = sorular.get(sira);
+        return soruu;
     }
 
     public String getCevap(int sira){
-        if(sira >= cevaplar.size()){
-           return  "OYUN BİTTİ";
-        }else
-            cevap = cevaplar.get(sira);
-            return cevap;
+        cevap = cevaplar.get(sira);
+        return cevap;
+    }
+
+    public String getOthers1(int sira){
+        other1 = others1.get(sira);
+        return other1;
+    }
+
+    public String getOthers2(int sira){
+        other2 = others2.get(sira);
+        return other2;
+    }
+
+    public String getOthers3(int sira){
+        other3 = others3.get(sira);
+        return other3;
     }
 }
 
